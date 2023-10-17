4385ed76546a4a21e85b11965c900e9771af1eab
Delara Galehdari <delara.galehdari@linaro.org>
2023-10-17 08:23:32 +0100

    add: Carousel for homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     ...defaultTheme,
     extend: {
@@ -22,5 +25,9 @@ module.exports = {
       spacing: "margin, padding",
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    // require("@tailwindcss/typography"),
+
+    require("tw-elements/dist/plugin.cjs"),
+  ],
 };
